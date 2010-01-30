84ac76119e1ac8b734c480775c3f7f40077d232f
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-01-30 12:07:27 +0900

    fixed

--- setup.sh
+++ setup.sh
@@ -2,25 +2,24 @@
 
 ln -s -f $HOME/Dropbox/Development $HOME/
 
-cd $HOME/Development/dotfiles/
+BASE=$HOME/Development/dotfiles/
 
-ln -s -f .profile $HOME/
-ln -s -f .zshrc $HOME/
-ln -s -f .vim $HOME/
-ln -s -f .vimrc $HOME/
-ln -s -f .vimperatorrc $HOME/
-ln -s -f .vimperator $HOME/
-ln -s -f .git $HOME/
-ln -s -f .gitconfig $HOME/
-ln -s -f .irbrc $HOME/
-ln -s -f .screen $HOME/
-ln -s -f .ssh $HOME/
-ln -s -f .irssi $HOME/
+ln -s -f $BASE.profile $HOME/
+ln -s -f $BASE.zshrc $HOME/
+ln -s -f $BASE.vim $HOME/
+ln -s -f $BASE.vimrc $HOME/
+ln -s -f $BASE.vimperatorrc $HOME/
+ln -s -f $BASE.vimperator $HOME/
+ln -s -f $BASE.gitconfig $HOME/
+ln -s -f $BASE.irbrc $HOME/
+ln -s -f $BASE.screen $HOME/
+ln -s -f $BASE.ssh $HOME/
+ln -s -f $BASE.irssi $HOME/
 
 OS=`uname`
 
 if [ $OS = Linux ]; then
-    ln -s -f .network $HOME/
+    ln -s -f $BASE.network $HOME/
 else
-    ln -s -f .inputrc $HOME/
+    ln -s -f $BASE.inputrc $HOME/
 fi
