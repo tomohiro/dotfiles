30c75bc0c8301cbc71542afd8720e18959bc06ef
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-07-22 15:54:09 +0900

    リロードした際にスクロールの位置も復元できるようにする

--- bin/reload_firefox.rb
+++ bin/reload_firefox.rb
@@ -6,7 +6,10 @@ config = {'Host' => 'localhost', 'Port' => 4242 }
 
 begin
   telnet = Net::Telnet.new(config)
-  telnet.puts 'content.location.reload(true)'
+  telnet.cmd 'x = content.window.pageXOffset'
+  telnet.cmd 'y = content.window.pageYOffset'
+  telnet.cmd 'BrowserReload()'
+  telnet.cmd 'content.window.scrollTo(x, y)'
   telnet.close
 rescue
   false
