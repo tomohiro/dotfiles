5b149207a2535f108d4b029c958ba9cc8b7e97be
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-05-02 00:51:58 +0900

    fixes

--- setup.sh
+++ setup.sh
@@ -1,35 +1,90 @@
 #!/bin/sh
 
-ln -s -f $HOME/Dropbox/Development $HOME/
+# Environment variable settings
 
-BASE=$HOME/Development/dotfiles/
+      RED="\033[2;31m"
+      GREEN="\033[2;32m"
+      YELLOW="\033[2;33m"
+      BLUE="\033[2;34m"
+      MAGENTA="\033[2;35m"
+      CYAN="\033[2;36m"
+      WHITE="\033[2;37m"
 
-ln -s -f {$BASE}bin $HOME/
-ln -s -f $BASE.profile $HOME/
-ln -s -f $BASE.zshrc $HOME/
-ln -s -f $BASE.zsh $HOME/
-ln -s -f $BASE.vim $HOME/
-ln -s -f $BASE.vimrc $HOME/
-ln -s -f $BASE.vimperatorrc $HOME/
-ln -s -f $BASE.vimperator $HOME/
-ln -s -f $BASE.irbrc $HOME/
-ln -s -f $BASE.screen $HOME/
-ln -s -f $BASE.screenrc $HOME/
-ln -s -f $BASE.ssh $HOME/
-ln -s -f $BASE.irssi $HOME/
-ln -s -f $BASE.vmc_target $HOME/
-ln -s -f $BASE.vmc_token $HOME/
-ln -s -f $BASE.pit $HOME/
-ln -s -f $BASE.todo $HOME/
-ln -s -f $BASE.todo.actions.d $HOME/
-ln -s -f $BASE.gem $HOME/
-ln -s -f $BASE.gitconfig $HOME/
-ln -s -f {$BASE}gitignore $HOME/.gitignore
+      OS=`uname`
+      DEV_PATH=$HOME/Dropbox/Development
+      DOTFILES_PATH=$DEV_PATH/dotfiles
+      SCRIPT_NAME=$DOTFILES_PATH/`basename $0`
 
-OS=`uname`
+      cd $DOTFILES_PATH
 
-if [ $OS = Linux ]; then
-    ln -s -f $BASE.fonts.conf $HOME/
-else
-    ln -s -f $BASE.inputrc $HOME/
-fi
+      echo "$SCRIPT_NAME\nStarting..."
+
+# Ignore files settings
+
+      IGNORES=( bin _vimperatorrc README.mkdn setup.sh )
+      case $OS in
+          Linux)
+              IGNORES[4]='profile'
+              ;;
+          Darwin)
+              IGNORES[4]='bash_profile'
+              IGNORES[5]='fonts.conf'
+              IGNORES[6]='gemrc'
+              ;;
+          *)
+      esac
+
+
+# Make base directories symlinks
+
+      echo "$YELLOW\n  Make base directories symlinks.$WHITE"
+
+      ln -s -f $DEV_PATH $HOME/
+      echo "    $BLUE[Dir]  $WHITE $HOME/${MAGENTA}Development$WHITE@ -> $DEV_PATH"
+
+      ln -s -f $DOTFILES_PATH/bin $HOME/
+      echo "    $BLUE[Dir]  $WHITE $HOME/${MAGENTA}bin$WHITE@ -> $DOTFILES_PATH/bin"
+
+
+
+# Make dotfiles symlinks
+
+      echo "$YELLOW\n  Make dotfiles symlinks.$WHITE"
+
+      for i in `ls $DOTFILES_PATH`; do
+          for ignore in ${IGNORES[@]}; do
+              if [ $i == $ignore ]; then
+                  continue 2
+              fi
+          done
+
+
+          if [ -h $HOME/.$i ]; then
+              rm $HOME/.$i
+              echo "    $RED[Del]  $WHITE $HOME/$MAGENTA.$i$WHITE@"
+          fi
+
+          ln -s $DOTFILES_PATH/$i $HOME/.$i
+
+          if [ -d $i ]; then
+              echo "    $BLUE[Dir]  $WHITE $HOME/$MAGENTA.$i$WHITE@ -> $DOTFILES_PATH/$i"
+          else
+              echo "    $CYAN[File] $WHITE $HOME/$MAGENTA.$i$WHITE@ -> $DOTFILES_PATH/$i"
+          fi
+      done
+
+
+
+# Executable permission applyed
+
+      echo "$YELLOW\n  Executable permission applyed.$WHITE"
+
+      BIN_DIRS=( bin .screen/scripts .irssi/scripts )
+      for bin in ${BIN_DIRS[@]}; do
+          chmod +x $HOME/$bin/*
+          echo "    $CYAN[Bin] +x $WHITE $HOME/$bin"
+      done
+
+
+
+      echo "\nFinished"
