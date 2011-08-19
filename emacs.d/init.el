874113e3c08e17ae2c078362b787cd4132451369
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-08-19 13:09:15 +0900

    add emacs.d

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,2 +1,11 @@
 (set-language-environment 'Japanese)
 (prefer-coding-system 'utf-8)
+
+
+(create-fontset-from-ascii-font "Inconsolata-15:weight=normal:slant=normal" nil "inconsolata")
+(set-fontset-font "fontset-inconsolata"
+                  'unicode
+                  (font-spec :family "Hiragino Kaku Gothic ProN" :size 15)
+                  nil
+                  'append)
+(add-to-list 'default-frame-alist '(font . "fontset-inconsolata"))
