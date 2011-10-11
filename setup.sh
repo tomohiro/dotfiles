77f042b5a6cd75a2f754b596d0cfd23847334025
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-10-11 17:30:30 +0900

    remove colors

--- setup.sh
+++ setup.sh
@@ -2,14 +2,6 @@
 
 # Setting environment variables
 
-      RED="\033[2;31m"
-      GREEN="\033[2;32m"
-      YELLOW="\033[2;33m"
-      BLUE="\033[2;34m"
-      MAGENTA="\033[2;35m"
-      CYAN="\033[2;36m"
-      WHITE="\033[2;37m"
-
       OS=`uname`
       DEV_PATH=$HOME/Dropbox/Development
       DOTFILES_PATH=$DEV_PATH/dotfiles
@@ -22,7 +14,7 @@
 # Setting ignore files
 
       IGNORES=(bin _vimperatorrc README.mkdn setup.sh)
-      case $OS in
+      case $KERNEL in
           Linux)  IGNORES=(${IGNORES[@]} profile) ;;
           Darwin) IGNORES=(${IGNORES[@]} bash_profile fonts.conf gemrc) ;;
       esac
@@ -31,19 +23,19 @@
 
 # Make base directories symlinks
 
-      echo "$YELLOW\n  Make base directories symlinks.$WHITE"
+      echo "\n  Make base directories symlinks."
 
       ln -s -f $DEV_PATH $HOME/
-      echo "    $BLUE[Dir]  $WHITE $HOME/${MAGENTA}Development$WHITE@ -> $DEV_PATH"
+      echo "    [Dir]   $HOME/Development@ -> $DEV_PATH"
 
       ln -s -f $DOTFILES_PATH/bin $HOME/
-      echo "    $BLUE[Dir]  $WHITE $HOME/${MAGENTA}bin$WHITE@ -> $DOTFILES_PATH/bin"
+      echo "    [Dir]   $HOME/bin@ -> $DOTFILES_PATH/bin"
 
 
 
 # Make dotfiles symlinks
 
-      echo "$YELLOW\n  Make dotfiles symlinks.$WHITE"
+      echo "\n  Make dotfiles symlinks."
 
       for i in `ls $DOTFILES_PATH`; do
           for ignore in ${IGNORES[@]}; do
@@ -55,15 +47,15 @@
 
           if [ -h $HOME/.$i ]; then
               rm $HOME/.$i
-              echo "    $RED[Del]  $WHITE $HOME/$MAGENTA.$i$WHITE@"
+              echo "    [Del]   $HOME/.$i@"
           fi
 
           ln -s $DOTFILES_PATH/$i $HOME/.$i
 
           if [ -d $i ]; then
-              echo "    $BLUE[Dir]  $WHITE $HOME/$MAGENTA.$i$WHITE@ -> $DOTFILES_PATH/$i"
+              echo "    [Dir]   $HOME/.$i@ -> $DOTFILES_PATH/$i"
           else
-              echo "    $CYAN[File] $WHITE $HOME/$MAGENTA.$i$WHITE@ -> $DOTFILES_PATH/$i"
+              echo "    [File]  $HOME/.$i@ -> $DOTFILES_PATH/$i"
           fi
       done
 
@@ -71,12 +63,12 @@
 
 # Apply execute permissions
 
-      echo "$YELLOW\n  Apply exexute permissions.$WHITE"
+      echo "\n  Apply exexute permissions."
 
       BIN_DIRS=( bin .screen/scripts .irssi/scripts )
       for bin in ${BIN_DIRS[@]}; do
           chmod +x $HOME/$bin/*
-          echo "    $CYAN[Bin] +x $WHITE $HOME/$bin"
+          echo "    [Bin] +x  $HOME/$bin"
       done
 
 
