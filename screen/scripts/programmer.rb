6f4cefb25f43e8e48c9987964936dfa2c2f4ecdc
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2012-08-09 20:27:15 +0900

    add motto

--- screen/scripts/programmer.rb
+++ screen/scripts/programmer.rb
@@ -3,11 +3,16 @@
 
 if __FILE__ == $0
   mottoes = [
+    "Programming or Die",
     "Shut the fuck up and write some code",
     "Coding is not crime, not coding is crime",
     "Don't Repeat Yourself",
     "Do It Yourself",
-    "Programming or Die"
+    "You Aren't Going to Need It",
+    "Keep It Simple, Stupid",
+    "Laziness, Impatience and Hubris",
+    "Tell, Don't Ask",
+    "Small is beautiful",
   ]
 
   puts "❝ #{mottoes.sample.upcase} ❞"
