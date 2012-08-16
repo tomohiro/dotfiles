cc9d36855159a8cb8db54adfe34b536428c8520f
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2012-08-16 17:17:01 +0900

    cleanup

--- screen/scripts/github_activity.rb
+++ screen/scripts/github_activity.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
+# Inspired by https://github.com/cho45/net-irc/blob/master/examples/gig.rb
 
 require 'open-uri'
 require 'time'
@@ -9,59 +10,45 @@ require 'nokogiri'
 module GitHub
   class Activity
     def initialize(user, token)
-      @user  = user
-      @token = token
+      @feeds = [
+        "https://github.com/#{user}.private.atom?token=#{token}",
+        "https://github.com/#{user}.private.actor.atom?token=#{token}"
+      ]
       @last  = Time.now
     end
 
-    def streaming
-      display 'GitHub Activity streaming...'
-
-      STDOUT.sync = true
+    def monitoring
       display 'Waiting new activities'
 
-      feeds = [
-        "https://github.com/#{@user}.private.atom?token=#{@token}",
-        "https://github.com/#{@user}.private.actor.atom?token=#{@token}"
-      ]
-
       loop do
-        begin
-          activities = []
-          feeds.each do |uri|
-            feed = Nokogiri::XML(open(uri))
-            feed.css('entry').each do |activity|
-              activities << {
-                :id       => activity.at('id').text,
-                :datetime => Time.parse(activity.at('published').text),
-                :title    => activity.at('title').text,
-                :author   => activity.at('author/name').text,
-                :link     => activity.at('link').attributes['href'].text
-              }
-            end
+        activities = []
+        @feeds.each do |uri|
+          Nokogiri::XML(open(uri)).search('entry').each do |activity|
+            activities << {
+              :id       => activity.at('id').text,
+              :title    => activity.at('title').text,
+              :author   => activity.at('author/name').text,
+              :link     => activity.at('link').attributes['href'].text,
+              :datetime => Time.parse(activity.at('published').text)
+            }
           end
-
-          activities.reverse_each do |activity|
-            next if activity[:datetime] <= @last
-            type = activity[:id][%r|tag:github.com,2008:(.+?)/\d+|, 1]
-            display "#{type} ⮁ #{activity[:title]} #{activity[:link]}"
-            sleep 10
-          end
-
-          @last = activities.first[:datetime]
-          sleep 30
-        rescue Exception => e
-          display e.inspect
-          e.backtrace.each { |l| puts "\t#{l}" }
-          sleep 10
         end
+
+        activities.reverse_each do |activity|
+          next if activity[:datetime] <= @last
+          type = activity[:id][/\d+:(.+)\//, 1]
+          display "#{type} ⮁ #{activity[:title]} #{activity[:link]}"
+        end
+
+        @last = activities.first[:datetime]
+        sleep 60
       end
     end
 
     private
-      def display(activity)
-        puts "⭠ GitHub ⮁ #{activity}"
-        sleep 5
+      def display(message)
+        puts "⭠ GitHub ⮁ #{message}"
+        sleep 10
       end
   end
 end
@@ -73,5 +60,6 @@ if __FILE__ == $0
     :token => 'your private feed atom token in GitHub'
   })
 
-  GitHub::Activity.new(config[:user], config[:token]).streaming
+  STDOUT.sync = true
+  GitHub::Activity.new(config[:user], config[:token]).monitoring
 end
