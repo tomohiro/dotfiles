f62f17875e65b1c92ce16760b0e9d1b87279ec45
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2012-08-16 17:16:47 +0900

    cleanup

--- screen/scripts/last.fm_recent_track.rb
+++ screen/scripts/last.fm_recent_track.rb
@@ -4,5 +4,4 @@ require 'open-uri'
 require 'rubygems'
 require 'nokogiri'
 
-xml = Nokogiri::XML(open('http://ws.audioscrobbler.com/1.0/user/tomohiro-taira/recenttracks.rss').read)
-puts (xml/'item/title').first.text
+puts Nokogiri::XML(open('http://ws.audioscrobbler.com/1.0/user/tomohiro-taira/recenttracks.rss')).search('item/title').first.text
