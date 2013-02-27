6f8a2895c48fa11628896dd33946b94c71729298
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-02-27 16:11:55 +0900

    add antigen

---
+++ zsh/autoload/antigen.zsh
@@ -0,0 +1,9 @@
+# [zsh-users/antigen · GitHub](https://github.com/zsh-users/antigen)
+
+source $HOME/Development/dotfiles/bundle/antigen/antigen.zsh
+
+# Load the oh-my-zsh's library.
+# antigen-lib
+
+# Syntax highlighting bundle.
+antigen-bundle zsh-users/zsh-syntax-highlighting
