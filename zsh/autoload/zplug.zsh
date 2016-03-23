ceba5c43e21b78d8eb8c6d43e92c9e4224b3e80a
Tomohiro TAIRA <tomohiro.t@gmail.com>
2016-03-23 16:13:36 +0900

    Update comments

--- zsh/autoload/zplug.zsh
+++ zsh/autoload/zplug.zsh
@@ -1,19 +1,22 @@
-export ZPLUG_HOME=$HOME/.zsh
-export ZPLUG_EXTERNAL=/dev/null
+#!/bin/zsh
+
+# Configure zplug options
+typeset -x ZPLUG_HOME=$HOME/.zsh
+typeset -x ZPLUG_EXTERNAL=/dev/null
+
+# Load zplug functions
 source $ZPLUG_HOME/repos/b4b4r07/zplug/zplug
 
+# Plugins
 zplug 'b4b4r07/zplug'
 zplug 'zsh-users/zsh-syntax-highlighting'
 zplug 'zsh-users/zsh-autosuggestions'
 zplug 'zsh-users/zsh-completions'
 zplug 'zsh-users/zsh-history-substring-search'
 
-# zplug check return true if all plugins are installed
-# Therefore, when it returns not true (thus false),
-# run zplug install
-if ! zplug check; then
-    zplug install
-fi
+# Install all Zsh plugins if these are not installed.
+zplug check || zplug install
 
-# Load Zsh plugins and which add to the PATH (Add `--verbose` to shows detail)
+# Load Zsh plugins and then add these to the PATH.
+# If you want to see detail, add `--verbose` option when runs `zplug load`.
 zplug load
