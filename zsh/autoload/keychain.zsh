08cd83931e09282a9b47c425b64ffe124be4c3eb
Tomohiro Taira <tomohiro.t@gmail.com>
2018-12-21 14:48:02 +0900

    Use macOS Keychain instead of keychain

--- zsh/autoload/keychain.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-# Configuration for Keychain
-#   - https://www.funtoo.org/Keychain
-if type keychain &> /dev/null; then
-  KEYCHAIN_HOME=$XDG_CACHE_HOME/keychain
-  SSH_AGENT=$KEYCHAIN_HOME/$(hostname)-sh
-  keychain -q $HOME/.ssh/id_rsa --dir $KEYCHAIN_HOME
-  source $SSH_AGENT
-fi
