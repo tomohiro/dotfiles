e99e72a537006cdb6209a17431f0a81d771a3a94
Tomohiro Taira <tomohiro.t@gmail.com>
2019-01-18 14:19:49 +0900

    Remove screen

--- screen/scripts/last.fm_recent_track.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'open-uri'
-require 'rubygems'
-require 'nokogiri'
-
-puts Nokogiri::XML(open('http://ws.audioscrobbler.com/1.0/user/tomohiro-taira/recenttracks.rss')).search('item/title').first.text
