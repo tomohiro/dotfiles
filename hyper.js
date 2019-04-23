e08dc223421d45cc332c77c7e4646d47e5a03893
Tomohiro Taira <tomohiro.t@gmail.com>
2019-04-24 00:21:02 +0900

    Remove unused options

--- hyper.js
+++ hyper.js
@@ -1,118 +1,33 @@
-// Future versions of Hyper may add additional config options,
-// which will not automatically be merged into this file.
+// Hyper configuration
 // See https://hyper.is#cfg for all currently supported options.
 
 module.exports = {
   config: {
-    // choose either `'stable'` for receiving highly polished,
-    // or `'canary'` for less polished but more frequent updates
     updateChannel: 'stable',
 
-    // default font size in pixels for all tabs
+    // Font
     fontSize: 12,
-
-    // font family with optional fallbacks
     fontFamily: 'Menlo for Powerline, monospace',
-
-    // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
-
-    // font weight for bold characters: 'normal' or 'bold'
     fontWeightBold: 'bold',
 
-    // line height as a relative unit
+    // Text
     lineHeight: 1,
-
-    // letter spacing as a relative unit
     letterSpacing: 0,
 
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    // Cursor
     cursorColor: '#C5C8C6',
-
-    // terminal text color under BLOCK cursor
     cursorAccentColor: '#000',
-
-    // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
     cursorShape: 'BLOCK',
-
-    // set to `true` (without backticks and without quotes) for blinking cursor
     cursorBlink: true,
 
-    // color of the text
-    foregroundColor: '#C5C8C6',
-
-    // terminal background color
-    // opacity is only supported on macOS
-    backgroundColor: '#1D1F21',
-
-    // terminal selection color
-    selectionColor: 'rgba(71,75,82,0.3)',
-
-    // border color (window, tabs)
-    borderColor: '#1D1F21',
-
-    // custom CSS to embed in the main window
-    css: '',
-
-    // custom CSS to embed in the terminal window
-    termCSS: '',
-
-    // if you're using a Linux setup which show native menus, set to false
-    // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: '',
-
-    // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: '',
-
-    // custom padding (CSS format, i.e.: `top right bottom left`)
-    // padding: '0px 5px',
+    // Appearance
+    selectionColor: 'rgba(71, 75, 82, 0.3)',
     padding: '0px 5px 15px 5px',
-
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
-    colors: {},
-
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    //
-    // Windows
-    // - Make sure to use a full path if the binary name doesn't work
-    // - Remove `--login` in shellArgs
-    //
-    // Bash on Windows
-    // - Example: `C:\\Windows\\System32\\bash.exe`
-    //
-    // PowerShell on Windows
-    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '',
-
-    // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
-    // by default `['--login']` will be used
-    shellArgs: ['--login'],
-
-    // for environment variables
-    env: {},
-
-    // set to `false` for no bell
     bell: 'SOUND',
 
-    // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
-    copyOnSelect: false,
-
-    // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
-    defaultSSHApp: true,
-
-    // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
-    // selection is present (`true` by default on Windows and disables the context menu feature)
-    // quickEdit: true,
-
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
+    // Selected text will automatically be copied to the clipboard
+    copyOnSelect: true,
 
     // Plugin configuration
     //  You can find awesome plugins here: https://github.com/bnb/awesome-hyper
@@ -130,13 +45,13 @@ module.exports = {
     },
   },
 
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
-  plugins: ['hyper-search', 'hyperline',  'hyper-iceberg'],
+  plugins: [
+    'hyper-one-dark',
+    'hyper-search',
+    'hyper-quit',
+    'hyperline',
+    'hyperlinks',
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
